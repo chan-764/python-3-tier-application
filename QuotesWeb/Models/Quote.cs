328b4fc984a2023cc6f5cf29b6fe08e0a964d109
chan-764 <chandanuikey19@gmail.com>
2025-02-05 16:23:46 +0530

    Create Quote.cs

---
+++ QuotesWeb/Models/Quote.cs
@@ -0,0 +1,9 @@
+namespace QuotesWeb.Models
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string QuoteText { get; set; }
+        public string Author { get; set; }
+    }
+}
