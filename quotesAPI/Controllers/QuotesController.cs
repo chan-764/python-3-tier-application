2350449145778ba378bfac45caace90371f2e362
chan-764 <chandanuikey19@gmail.com>
2025-02-06 15:18:06 +0530

    Update QuotesController.cs

--- quotesAPI/Controllers/QuotesController.cs
+++ quotesAPI/Controllers/QuotesController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using QuotesApi.Data;
 using QuotesApi.Models;
 
