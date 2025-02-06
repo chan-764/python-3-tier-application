36f0cd0ac5df053e640a3fa4a7bb2319580cebca
chan-764 <chandanuikey19@gmail.com>
2025-02-06 22:49:42 +0530

    Delete QuotesWeb directory

--- QuotesWeb/Models/Quote.cs
+++
@@ -1,9 +0,0 @@
-namespace QuotesWeb.Models
-{
-    public class Quote
-    {
-        public int Id { get; set; }
-        public string QuoteText { get; set; }
-        public string Author { get; set; }
-    }
-}
