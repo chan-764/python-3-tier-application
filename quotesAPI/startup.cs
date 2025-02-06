c80ff53dc8f4e6eaf096a1d059031775f5681f1f
chan-764 <chandanuikey19@gmail.com>
2025-02-06 14:46:29 +0530

    Update startup.cs

--- quotesAPI/startup.cs
+++ quotesAPI/startup.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
-using QuotesAPI.Data;
+using QuotesApi.Data;
 
 namespace QuotesAPI
 {
