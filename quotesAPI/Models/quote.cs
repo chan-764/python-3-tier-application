b7d6e14105523038f26e8219649d272336e4445d
chan-764 <chandanuikey19@gmail.com>
2025-02-08 22:52:13 +0530

    Update quote.cs

--- quotesAPI/Models/quote.cs
+++ quotesAPI/Models/quote.cs
@@ -1,9 +1,12 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace QuotesApi.Models
 {
+    [Table("quotes")]  // Ensure it matches the actual MySQL table name
     public class Quote
     {
         public int Id { get; set; }
-        public string Content { get; set; }
+        public string QuoteText { get; set; }
         public string Author { get; set; }
     }
 }
