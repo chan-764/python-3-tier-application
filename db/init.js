513a2848007d03c5e87812f62b789300b2697154
chan-764 <chandanuikey19@gmail.com>
2025-02-08 22:57:57 +0530

    Update init.js

--- db/init.js
+++ db/init.js
@@ -1,24 +1,16 @@
+-- Create database if it does not exist
+CREATE DATABASE IF NOT EXISTS quotesdb;
+USE quotesdb;
 
+-- Create table for storing quotes
+CREATE TABLE IF NOT EXISTS quotes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    content TEXT NOT NULL,
+    author VARCHAR(255) NOT NULL
+);
 
-// Access the 'quotes' collection (MongoDB will create this if it doesn't exist)
-db.quotes.insertMany([
-    {
-        quote: "Be the change you want to see in the world.",
-        author: "Ghandi"
-    },
-    {
-        quote: "Master patterns in life, and you'll never suffer is the secret",
-        author: "someone"
-    }
-]);
-
-
-
-
-
-
-
-
-
-
-
+-- Insert sample data
+INSERT INTO quotes (content, author) VALUES
+('The best way to predict the future is to invent it.', 'Alan Kay'),
+('Code is like humor. When you have to explain it, it’s bad.', 'Cory House'),
+('Simplicity is the soul of efficiency.', 'Austin Freeman');
